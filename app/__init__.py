5e41a1057b76c3ea9b8f38262c72ec4a1f81aaab
kempybizz-dotcom <kempybizz@gmail.com>
2025-12-03 17:55:46 +0000

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,1 @@
-"""
-API Routes Package
-Exports all route modules for easy import
-"""
 
-from app.routes import onboarding, stripe_webhooks, testing
-
-__all__ = ['onboarding', 'stripe_webhooks', 'testing']
